06497db5e53649b3812c0a75013a37417806bb0c
Samuel liu <sliu929@aucklanduni.ac.nz>
2022-03-07 14:08:01 +1300

    Implement encounter.model.ts
    Added a mongoose model to represent an encounter in MongoDB database.

---
+++ backend/src/models/encounter.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+interface EncounterModel {
+  date: Date,
+  location: string,
+  description: string,
+  persons: []
+}
+
+const schema = new Schema<EncounterModel>({
+  date: { type: Date, default: Date.now },
+  location: { type: String, required: false },
+  description: { type: String, required: true },
+  persons: { type: [Schema.Types.ObjectId], required: true }
+});
+
+export default model<EncounterModel>('Encounter', schema);
