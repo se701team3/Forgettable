cd404e03655f61729f3943fbfcb5d34ab060b730
Syon Divekar <57572181+sdiv877@users.noreply.github.com>
2022-03-09 20:49:09 +1300

    Implement POST person/create endpoint (#81)
    * Add http-status
    
    * Reconfigure routes/port
    
    * Add POST persons/create endpoint
    
    * Add POST type
    
    * Go back to using next for error logging
    
    Also switch return type of POST to Promise<void>
    
    * Switch to using .env port value

--- backend/src/controllers/person.controller.ts
+++ backend/src/controllers/person.controller.ts
@@ -1,30 +1,23 @@
 /**
  * Controller contains high-level operations using services, consumed by routes
  */
-import { NextFunction, Request, Response } from 'express';
-import { createPerson as createPersonService } from '../services/person.service';
+import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status';
+import personService from '../services/person.service';
+import logger from '../utils/logger';
 
-export const getRandomJson = async (
+import { POST } from './controller.types';
+
+export const createPerson: POST = async (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ): Promise<void> => {
-  try {
-    res.json({ someString: 'hello world' });
-  } catch (e) {
-    next(e);
-  }
-};
+  logger.info("POST /persons/create request from frontend");
 
-export const createPerson = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
-  const { name, organisation } = req.body;
   try {
-    await createPersonService({ name, organisation });
-    res.status(200).end();
+    const person = await personService.createPerson(req.body);
+    res.status(httpStatus.CREATED).send(person);
   } catch (e) {
     next(e);
   }
