3c2649dc4cd114722c3c32441076af6ac9d76177
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2022-03-10 16:11:49 +1300

    Created button component

--- forgettable-frontend/src/components/CustomButton/CustomButton.js
+++ forgettable-frontend/src/components/CustomButton/CustomButton.js
@@ -7,7 +7,6 @@ function CustomButton(props) {
     btnText, onClick, className,
   } = props;
   return (
-  // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div className={classnames(classes.Button, className)} onClick={onClick}>
       <p className={classes.Text}>{ btnText }</p>
     </div>
