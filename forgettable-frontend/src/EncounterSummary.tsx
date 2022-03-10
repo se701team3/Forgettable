7e63feaf4d219da1f505a91ef5d090679b57b2a0
rdum464 <rushaandumasia@gmail.com>
2022-03-11 10:09:42 +1300

    updated colors

--- forgettable-frontend/src/EncounterSummary.tsx
+++ forgettable-frontend/src/EncounterSummary.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import { Avatar } from '@mui/material';
+import {Avatar} from '@mui/material';
 import './EncounterSummary.css';
 
 export default function EncounterSummary(props: { Name: any; Date: any; Description: any; Summary: any; }) {
   const {
     Name, Date, Description, Summary,
   } = props;
+
   return (
     <div className="container">
       <Card
         className="card"
-        sx={{ maxWidth: 220, maxHeight: 240 }}
+        sx={{maxWidth: 220, maxHeight: 240, bgcolor: 'var(--lcard)'}}
       >
         <div className="header">
           <Avatar
             alt="Remy Sharp"
             src="Avatar.png"
-            sx={{ width: 70, height: 70 }}
+            sx={{width: 70, height: 70}}
           />
           <div className="nameDesc">
             <div>{Name}</div>
