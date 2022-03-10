4152872bd0896725c54a6a4406291088daf95d85
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-11 00:00:04 +1300

    add basic elements of card

--- forgettable-frontend/src/components/PersonCard/PersonCard.js
+++ forgettable-frontend/src/components/PersonCard/PersonCard.js
@@ -6,9 +6,28 @@ const PersonCard = (props) => {
   return (
 
     <div className={classes.PersonCard}>
-      <div className={classes.AvatarContainer}>
-      </div>
-      <div className={classes.InformationContainer}>
+      <div className={classes.ContentContainer}>
+
+        <Avatar className={classes.Avatar}>JD</Avatar>
+        <div className={classes.InformationContainer}>
+
+          <div className={classes.MainInformationContainer}>
+            <div className={classes.IdentityInformation}>
+              <h2>Marley George</h2>
+              <p>Adipsicing sit lectus</p>
+            </div>
+
+          </div>
+          <div className={classes.SupplementaryInformationContainer}>
+            <div className={classes.Encounters}>
+                  Encounters: 3 times
+            </div>
+            <div className={classes.LastMet}>
+                Date last met: 06/06/2020
+            </div>
+            <div className={classes.SocialMediaContainer}>x</div>
+          </div>
+        </div>
       </div>
     </div>
 
