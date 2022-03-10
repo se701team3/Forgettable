ee2bb79f17f478c07972884e56318c3c37f89b8d
schi314 <schi314@aucklanduni.ac.nz>
2022-03-11 10:36:39 +1300

    Add tests for POST /encounter/<encounterID>
    Test cases to check encounter entries are created as expected if response parameters and payloads are correctly given. This test is not complete because it requires Firebase auth token mocks.

--- backend/src/routes/__test__/encounter.route.test.ts
+++ backend/src/routes/__test__/encounter.route.test.ts
@@ -1,7 +1,8 @@
+import httpStatus from 'http-status';
 import databaseOperations from '../../utils/test/db-handler';
+import Encounter, { EncounterModel } from '../../models/encounter.model';
+import Person, { PersonModel } from '../../models/person.model';
 import app from '../../server';
-import {EncounterModel} from "../../models/encounter.model";
-import httpStatus from "http-status";
 
 const supertest = require('supertest');
 
@@ -67,3 +68,138 @@ describe('encounter ', () => {
         .expect(httpStatus.NOT_FOUND)
   });
 });
+const person1Data: PersonModel = {
+    first_name: 'Ray',
+    last_name: 'Ping',
+    interests: ['video games', 'hockey'],
+    organisation: 'helloc',
+    time_added: new Date('2022-01-01'),
+    how_we_met: 'Hockey club',
+    birthday: new Date('2002-12-12'),
+    encounters: [] as any,
+};
+
+const person2Data: PersonModel = {
+    first_name: 'Adam',
+    last_name: 'Bong',
+    interests: ['badminton', 'golf'],
+    organisation: 'helloc',
+    time_added: new Date('2022-02-23'),
+    how_we_met: 'Skype',
+    birthday: new Date('2001-07-16'),
+    encounters: [] as any,
+}
+
+const encounter1Data: EncounterModel = {
+    date: new Date('2022-02-23'),
+    description: 'Met at a cafe',
+    location: 'Auckland',
+    persons: [] as any
+}
+
+const encounter2Data: EncounterModel = {
+    date: new Date('2022-02-24'),
+    description: 'Had lunch together',
+    location: 'Auckland',
+    persons: [] as any
+}
+
+//Bad encounter entry as persons are not given
+const encounter3Data = {
+    date: new Date('2022-05-25'),
+    description: 'Play badminton together',
+    location: 'Auckland',
+    persons: null
+}
+
+const encounter4Data = {
+    description: 'Play badminton together',
+    location: 'Auckland',
+    persons: []
+}
+
+const encounter5Data = {
+    date: new Date('2022-05-25'),
+    location: 'Auckland',
+    persons: []
+}
+
+const encounter6Data = {
+    date: new Date('2022-05-25'),
+    description: 'Play badminton together',
+    persons: []
+}
+
+describe('POST /encounter/:encounterID', () => {
+    it('Successfully creating an encounter with empty persons array', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter1Data)
+            .expect(httpStatus.CREATED);
+    })
+
+    it('Encounter stored in database when request is successful', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter1Data)
+            .expect(httpStatus.CREATED);
+
+        //TODO: needs to get the encounter to make a check that the encounter was successfully made
+    })
+
+    it('Encounter not stored in database when request is unsuccessful', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter3Data)
+            .expect(httpStatus.CREATED);
+
+        //TODO: needs to get encounter to make a check that the encounter was not stored in database
+    })
+
+    it('Successfully creating an encounter with 2 persons', async () => {
+        const { body: person1 } = await supertest(app).post('/api/persons')
+            .set('Accept', 'application/json')
+            .send(person1Data);
+
+        const { body: person2 } = await supertest(app).post('/api/persons')
+            .set('Accept', 'application/json')
+            .send(person2Data);
+
+        encounter1Data.persons.push(person1._id);
+        encounter1Data.persons.push(person2._id);
+
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter1Data)
+            .expect(httpStatus.CREATED);
+    })
+
+    it('Failed to create an encounter without a persons field', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter3Data)
+            .expect(httpStatus.BAD_REQUEST)
+    })
+
+    it('Successfully creating an encounter without a date field', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter4Data)
+            .expect(httpStatus.CREATED);
+    })
+
+    it('Failed to create an encounter without a description field', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter5Data)
+            .expect(httpStatus.BAD_REQUEST);
+    })
+
+    it('Successfuly creating an encounter without a location field', async () => {
+        await supertest(app).post('/api/encounters')
+            .set('Accept', 'application/json')
+            .send(encounter6Data)
+            .expect(httpStatus.CREATED);
+    })
+
+})
