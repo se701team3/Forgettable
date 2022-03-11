ae3716567368acd419703b2728f8a1eede1e66fe
rainasong <rainasong0509@gmail.com>
2022-03-11 17:26:22 +1300

    Remove unused container

--- forgettable-frontend/src/components/EncounterCard/EncounterCard.js
+++ forgettable-frontend/src/components/EncounterCard/EncounterCard.js
@@ -4,7 +4,7 @@ import Card from '@mui/material/Card';
 import CustomButton from '../CustomButton/CustomButton';
 import {AvatarGroup, Avatar} from '@mui/material';
 import classes from './EncounterCard.module.css';
-import stringAvatar from './helper';
+import stringAvatar from '../../functions/helper';
 
 const DELETE = 'Delete';
 const DATE_WE_MET = 'Date we met: ';
@@ -15,48 +15,46 @@ export default function EncounterCard(props) {
   const isMultiplePerson = persons.length > 1;
 
   return (
-    <div className={classes.Container}>
-      <Card className={isInitialEncounter ? classes.Card_special : classes.Card}>
-        <div className={classes.Card_content}>
-          <div onClick={onClick}>
-            {isInitialEncounter && <section className={classes.Initial_label_container}>
-              <div className={classes.Initial_label}>First Encounter!</div>
-            </section>}
-            <section className={classes.Header}>
-              <div className={classes.Title}>
-                {title}
-              </div>
-              <div className={classes.Profile_container}>
-                <AvatarGroup max={4} className={isMultiplePerson ? classes.Avatar_multiple : classes.Avatar_inline}>
-                  {persons.map((person, index) => {
-                    return (
-                      <div key={index}>
-                        <Avatar key={index} className={classes.Avatar} {...stringAvatar(`${person.first_name} ${person.last_name}`)} />
-                        {!isMultiplePerson && <div className={classes.Avatar_name}>
-                          {person.first_name}
-                        </div>}
-                      </div>
-                    );
-                  },
-                  )}
-                </AvatarGroup>
-              </div>
-            </section>
-            <section className={classes.Body}>
-              {details}
-            </section>
-          </div>
-          <section className={classes.Footer}>
-            <div className={classes.Date}>
-              {DATE_WE_MET}<div>{date}</div>
+    <Card className={isInitialEncounter ? classes.Card_special : classes.Card}>
+      <div className={classes.Card_content}>
+        <div onClick={onClick}>
+          {isInitialEncounter && <section className={classes.Initial_label_container}>
+            <div className={classes.Initial_label}>First Encounter!</div>
+          </section>}
+          <section className={classes.Header}>
+            <div className={classes.Title}>
+              {title}
             </div>
-            <div className={classes.Location}>
-              {WHERE_WE_MET}<div>{location}</div>
+            <div className={classes.Profile_container}>
+              <AvatarGroup max={4} className={isMultiplePerson ? classes.Avatar_multiple : classes.Avatar_inline}>
+                {persons.map((person, index) => {
+                  return (
+                    <div key={index}>
+                      <Avatar key={index} className={classes.Avatar} {...stringAvatar(`${person.first_name} ${person.last_name}`)} />
+                      {!isMultiplePerson && <div className={classes.Avatar_name}>
+                        {person.first_name}
+                      </div>}
+                    </div>
+                  );
+                },
+                )}
+              </AvatarGroup>
             </div>
-            <CustomButton className={classes.Button} btnText={DELETE} onClick={onDelete}/>
+          </section>
+          <section className={classes.Body}>
+            {details}
           </section>
         </div>
-      </Card>
-    </div>
+        <section className={classes.Footer}>
+          <div className={classes.Date}>
+            {DATE_WE_MET}<div>{date}</div>
+          </div>
+          <div className={classes.Location}>
+            {WHERE_WE_MET}<div>{location}</div>
+          </div>
+          <CustomButton className={classes.Button} btnText={DELETE} onClick={onDelete}/>
+        </section>
+      </div>
+    </Card>
   );
 }
