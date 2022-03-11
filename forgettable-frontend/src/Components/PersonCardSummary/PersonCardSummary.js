ab2ac20731500216773ebff00688b8f1fc4f67d7
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-11 17:55:42 +1300

    add files for small ver person card

---
+++ forgettable-frontend/src/Components/PersonCardSummary/PersonCardSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './PersonCardSummary.module.css';
+
+const PersonCardSummary = (props) => {
+  return (
+
+    <div className={classes.PersonCardSummary}>
+      <div className={classes.ContentContainer}>
+
+
+      </div>
+
+    </div>
+
+
+  );
+};
+
+export default PersonCardSummary;
