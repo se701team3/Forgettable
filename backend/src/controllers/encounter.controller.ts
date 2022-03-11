d15a91b868e50d7ded781424de3546ea1348af44
hkim532 <68993476+hajineats@users.noreply.github.com>
2022-03-12 01:01:28 +1300

    PUT encounter/:id implementation and test

--- backend/src/controllers/encounter.controller.ts
+++ backend/src/controllers/encounter.controller.ts
@@ -6,27 +6,52 @@
  import EncounterModel from '../models/encounter.model';
  import encounterService from '../services/encounter.service';
  import logger from '../utils/logger';
+import * as http from "http";
+import httpStatus from "http-status";
  
  export const createEncounter = async (
    req: Request,
    res: Response,
    next: NextFunction,
  ): Promise<void> => {
-   logger.info("POST /encounter/create request from frontend");
+   logger.info("POST /encounter request from frontend");
  
    try {
      // Grab the data from the req
      const encounterReq = getEncounterFromReqBody(req.body);
- 
+
      // Pass data to service and attempt to save
      const createdEncounter = await encounterService.createEncounter(encounterReq);
- 
      // Notify frontend that the operation was successful
-     res.sendStatus(200);
+     res.status(httpStatus.CREATED).json(createdEncounter).end();
    } catch (e) {
      next(e);
    }
  };
+export const updateEncounter = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> => {
+    logger.info("PUT /encounter/:id request from frontend");
+
+    try {
+        // Grab the data from the req
+        const encounterReq = getEncounterFromReqBody(req.body);
+
+        // Pass data to service and attempt to save
+        const updatedEncounter = await encounterService.updateEncounter(req.params.id, encounterReq);
+        if(!updatedEncounter){
+            return res.sendStatus(httpStatus.NOT_FOUND).end();
+        }
+
+        // Notify frontend that the operation was successful
+        return res.sendStatus(httpStatus.NO_CONTENT).end();
+    } catch (e) {
+        next(e);
+    }
+};
+
  
  // Util function that won't be needed regularly
 const getEncounterFromReqBody = (body: any) => {
