632e481f92cae9a4e1bccd1aa5f3fb14f652f181
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-11 19:29:36 +1300

    render avatar for small card

--- forgettable-frontend/src/Components/PersonCardSummary/PersonCardSummary.js
+++ forgettable-frontend/src/Components/PersonCardSummary/PersonCardSummary.js
@@ -1,3 +1,4 @@
+import {Avatar} from '@mui/material';
 import React from 'react';
 import classes from './PersonCardSummary.module.css';
 
@@ -6,10 +7,26 @@ const PersonCardSummary = (props) => {
 
     <div className={classes.PersonCardSummary}>
       <div className={classes.ContentContainer}>
+        <Avatar
+          alt={'Person Name'}
+          // this style is written inline because MUI does not support className
+          style={{
+            height: '70px',
+            width: '70px',
+            marginRight: '28px',
+            backgroundColor:
+                getComputedStyle(document.body).getPropertyValue('--prmry'),
+            fontSize:
+                getComputedStyle(document.body)
+                    .getPropertyValue('--text-xxlarge')
+            ,
+          }}
+          src={props.img}
+        />
+        <div className={classes.TextInfoContainer}>
 
-
+        </div>
       </div>
-
     </div>
 
 
