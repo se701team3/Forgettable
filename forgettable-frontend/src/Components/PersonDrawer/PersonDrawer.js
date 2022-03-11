4ec827a166a4b0d8e3d77272f556b0c3d576752e
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-12 00:38:32 +1300

    add props to person drawer

--- forgettable-frontend/src/Components/PersonDrawer/PersonDrawer.js
+++ forgettable-frontend/src/Components/PersonDrawer/PersonDrawer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import classes from './PersonDrawer.module.css';
 import {Avatar, Drawer} from '@mui/material';
+import PropTypes from 'prop-types';
+import {getFirstMetTimeString} from '../../functions/dateFormatter';
 
 const PersonDrawer = (props) => {
+  const unknownDetail = <span className={classes.UnknownText}>Unknown</span>;
+
   return (
     <Drawer
       sx={{
@@ -20,7 +24,7 @@ const PersonDrawer = (props) => {
       <div className={classes.PersonDrawer}>
         <div className={classes.ContentContainer}>
           <Avatar
-            alt={'Name'}
+            alt={props.name}
             // this style is written inline because MUI does not support className
             style={{
               height: '200px',
@@ -34,16 +38,30 @@ const PersonDrawer = (props) => {
             src={props.img}
           />
           <div className={classes.InfoHeader}>
-            <h1>Name Person</h1>
-            <h2>First met some time</h2>
+            <h1>{props.name}</h1>
+            <h2>First met {getFirstMetTimeString(props.firstMet)}</h2>
           </div>
           <div className={classes.InfoContent}>
-              content
+            <p>Age: {props.birthday? <span className={classes.KnownText}>34</span> : unknownDetail}</p>
+            <p>Gender: {props.gender ? <span>Female</span> : unknownDetail}</p>
+            <p>Birthday: {props.birthday ? <span>13/12/2000</span> : unknownDetail}</p>
+            <p>Organisation: {props.organisation ? <span>Google</span> : unknownDetail}</p>
+            <p>Location: {props.location ? <span>Sydney</span> : unknownDetail}</p>
+            <p>Date first met: {props.firstMet ? <span>13/12/2009</span> : unknownDetail}</p>
+            <p>Their interests: {props.interests ? <span>Sydney</span> : unknownDetail}</p>
           </div>
         </div>
       </div>
     </Drawer>
   );
+};
+
+PersonDrawer.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string,
+  socialMedias: PropTypes.arrayOf(PropTypes.string),
+  firstMet: PropTypes.instanceOf(Date),
 };
 
 export default PersonDrawer;
