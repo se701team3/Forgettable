e57a7fe21258c7512a5750a86adfc9accb772514
jyao244 <jyao244@aucklanduni.ac.nz>
2022-03-11 17:47:32 +1300

    fix css file
    fix button1 button2 issue and add toolbar

--- forgettable-frontend/src/components/Dialog/Dialog.js
+++ forgettable-frontend/src/components/Dialog/Dialog.js
@@ -2,13 +2,13 @@ import React from 'react';
 import classes from './Dialog.module.css';
 
 function Dialog(props) {
-  const {title, desc, cancalEvent, confirmEvent} = props;
+  const {title, description, cancalEvent, confirmEvent} = props;
   return <div className={classes.dialog}>
     <div className={classes.title}>{title}</div>
-    <div className={classes.desc}>{desc}</div>
+    <div className={classes.description}>{description}</div>
     <div className={classes.toolbar}>
-      <button className={classes.button1} onClick={cancalEvent}>Cancal</button>
-      <button className={classes.button2} onClick={confirmEvent}>Confirm</button>
+      <button className={classes.button} onClick={cancalEvent}>Cancal</button>
+      <button className={classes.button} onClick={confirmEvent}>Confirm</button>
     </div>
   </div>;
 }
