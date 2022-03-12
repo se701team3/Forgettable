c4ce6fb62f1ca35a4771809afc2814f73bb466c6
jimwang6012 <68952466+jimwang6012@users.noreply.github.com>
2022-03-12 23:04:00 +1300

    Added Firebase Authorization to middleware (#123)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,16 @@
-import express from 'express';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import routes from './routes';
-import config from './utils/config';
-import logger from './utils/logger';
-import middleware from './utils/middleware';
-import app from './server';
+import { connect } from "mongoose";
+import config from "./utils/config";
+import logger from "./utils/logger";
+import app from "./server";
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   connect(config.MONGODB_URI)
-    .then(() => logger.info('connected to MongoDB'))
-    .catch((err) => logger.error('error connecting to mongodb:', err.message));
+    .then(() => logger.info("connected to MongoDB"))
+    .catch((err) => logger.error("error connecting to mongodb:", err.message));
 }
 
-app.listen(config.PORT, () => logger.info(`App server listening on port ${config.PORT}!`));
+app.listen(config.PORT, () =>
+  logger.info(`App server listening on port ${config.PORT}!`)
+);
 
 export default app;
