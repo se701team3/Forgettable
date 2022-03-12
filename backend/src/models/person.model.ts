069adfff339c5137bd790dbeaa0a7723d393d18d
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-12 15:14:17 +1300

    Add extra fields to Person model

--- backend/src/models/person.model.ts
+++ backend/src/models/person.model.ts
@@ -6,20 +6,28 @@ import mongoose, { Schema, model } from 'mongoose';
 export interface PersonModel {
   full_name: string,
   birthday: Date,
+  gender: string,
+  location: string,
+  date_met: Date,
+  how_we_met: string,
   interests: string[],
   organisation: string,
-  time_added: Date,
-  how_we_met: string,
-  encounters: mongoose.Types.ObjectId[]
+  social_media: Map<string, string>,
+  image: string
+  encounters: mongoose.Types.ObjectId[],
 }
 
 const schema = new Schema<PersonModel>({
   full_name: { type: String, required: true },
   birthday: { type: Date, required: false },
+  gender: { type: String, required: false },
+  location: { type: String, required: false },
+  date_met: { type: Date, required: false },
+  how_we_met: { type: String, required: false },
   interests: { type: [String], required: false },
   organisation: { type: String, required: false },
-  time_added: { type: Date, default: Date.now, required: true },
-  how_we_met: { type: String, required: false },
+  social_media: { type: Map, of: String, required: false },
+  image: { type: String, required: false },
   encounters: { type: [mongoose.Types.ObjectId], required: false },
 });
 
