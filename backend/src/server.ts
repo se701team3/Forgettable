7fda1a6670daa51fd41bace8bbc69e465ea5d242
schi314 <schi314@aucklanduni.ac.nz>
2022-03-12 15:27:07 +1300

    Fixes address in use error for multiple test suites
    Server attempted to listen to the same port multiple times when running test suites. Express app is abstracted into another file and test suites uses the app without listening to port.

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import middleware from './utils/middleware';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+app.use(middleware.errorHandler);
+
+export default app;
