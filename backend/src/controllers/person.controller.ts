d24fe39b13068b82731f71d7733814ba550cbe79
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-13 17:49:48 +1300

    Add auth to endpoint

--- backend/src/controllers/person.controller.ts
+++ backend/src/controllers/person.controller.ts
@@ -3,6 +3,7 @@
  */
 import { Request, Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
+import userService from '../services/user.service';
 import personService from '../services/person.service';
 import logger from '../utils/logger';
 
@@ -30,9 +31,20 @@ export const getAllPeople = async (
 ): Promise<void> => {
   logger.info('GET /persons request from frontend');
 
+  const auth_id = req.headers.authorization?.["user_id"];
+  const user = await userService.getUserByAuthId(auth_id);
+
   try {
-    const people = await personService.getPeople(req.query);
-    res.status(httpStatus.OK).json(people);
+    if (!user) {
+      res.status(httpStatus.NOT_FOUND).end();
+    } else {
+      if (!user.persons.length) {
+        res.status(httpStatus.NO_CONTENT).end();
+      } else {
+        const foundUserPersons = await personService.getPeople(req.query, user.persons);
+        res.status(httpStatus.OK).json(foundUserPersons).end();
+      }
+    }
   } catch (e) {
     next(e);
   }
--- backend/src/services/person.service.ts
+++ backend/src/services/person.service.ts
@@ -1,6 +1,7 @@
 /**
  * Service contains database operations
  */
+import mongoose from 'mongoose';
 import Person, { PersonModel } from '../models/person.model';
 import logger from '../utils/logger';
 
@@ -14,15 +15,26 @@ const createPerson = async (personDetails: PersonModel) => {
  * Note that .clone is necessary to avoid error 'Query was already executed'
  * Refer to section 'Duplicate Query Execution under https://mongoosejs.com/docs/migrating_to_6.html
  */
-const getPeople = async (queryParams: any) => {
-  if (Object.keys(queryParams).length === 0) {
-    return await Person.find(() => true).clone();
-  } else {
+const getPeople = async (queryParams: any, userPersons: mongoose.Types.ObjectId[]) => {
+  // Log query params if received
+  if (Object.keys(queryParams).length > 0) {
     logger.info('Query params received:');
     logger.info(queryParams);
-
-    return await Person.find({...queryParams}, () => true).clone();
   }
+
+  // Get all persons from the db that belong to the user
+  let foundUserPersons =  await Person.find({ _id: { $in: userPersons } });
+
+  // Filter them by query params (only works with exact strings)
+  let filteredPersons = foundUserPersons.filter(function(person) {
+    for (var key in queryParams) {
+      if (person[key] === undefined || person[key] != queryParams[key])
+        return false;
+    }
+    return true; 
+  });
+
+  return filteredPersons;
 };
 
 const personService = {
