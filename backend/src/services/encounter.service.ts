69d1ce2817c63231d936cfa5e4847126367a6d76
Hajin Kim <68993476+hajineats@users.noreply.github.com>
2022-03-14 01:47:37 +1300

    Implemented PUT /api/encounter/:id  (#132)
    * PUT encounter/:id implementation and test
    
    * update test for encounter
    
    * add authorization check

--- backend/src/services/encounter.service.ts
+++ backend/src/services/encounter.service.ts
@@ -1,13 +1,20 @@
 import Encounter, {EncounterModel} from '../models/encounter.model';
 
-export const createEncounter = async (encounterDetails: EncounterModel) => {
+const createEncounter = async (encounterDetails: EncounterModel) => {
     const encounter = new Encounter(encounterDetails);
     await encounter.save();
     return encounter;
 };
 
+const updateEncounter = async (objectID: string, encounterDetails: EncounterModel)=>{
+    console.log(objectID)
+    const updatedEncounter = await Encounter.findByIdAndUpdate(objectID, encounterDetails, {new: true});
+    return updatedEncounter;
+}
+
 const encounterService = {
-    createEncounter
+    createEncounter,
+    updateEncounter
   }
 
 export default encounterService;
