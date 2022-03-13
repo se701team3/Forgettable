c7479767b4625c150254ac84830bc1dbef723155
ckar364 <ckar364@aucklanduni.ac.nz>
2022-03-14 09:12:33 +1300

    chore: modify file path for NavBar

--- forgettable-frontend/src/index.tsx
+++ forgettable-frontend/src/index.tsx
@@ -8,7 +8,7 @@ import {
 import './index.css';
 import App from './pages/home/App';
 import reportWebVitals from './reportWebVitals';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './Components/NavBar/NavBar';
 import Settings from './pages/settings/settings';
 import People from './pages/people/people';
 import Encounters from './pages/encounters/encounters';
