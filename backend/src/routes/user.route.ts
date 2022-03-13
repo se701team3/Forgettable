f2d7b2f8a727e14178c6deab476fcc3c576b5248
Joel <joelkahuhutch@gmail.com>
2022-03-14 00:53:42 +1300

    fix/feat: lint issues, added PUT peoples endpoint

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,13 +1,12 @@
 /**
  * Route configures endpoints, and attaches controller as an action to each route's REST methods
  */
- import { Router } from 'express';
- import { createUser, getUser } from '../controllers/user.controller';
- 
- const routes = Router();
- 
- routes.post('/', createUser);
- routes.get('/', getUser)
- 
- export default routes;
- 
+import { Router } from 'express';
+import { createUser, getUser } from '../controllers/user.controller';
+
+const routes = Router();
+
+routes.post('/', createUser);
+routes.get('/', getUser);
+
+export default routes;
