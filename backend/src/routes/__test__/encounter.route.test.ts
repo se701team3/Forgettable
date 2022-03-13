69a2a54bdc7aafa642282bee9c266eaec2666a53
schi314 <schi314@aucklanduni.ac.nz>
2022-03-14 11:27:12 +1300

    Update changes from main

--- backend/src/routes/__test__/encounter.route.test.ts
+++ backend/src/routes/__test__/encounter.route.test.ts
@@ -1,8 +1,8 @@
-import httpStatus from 'http-status';
 import databaseOperations from '../../utils/test/db-handler';
-import Encounter, { EncounterModel } from '../../models/encounter.model';
-import Person, { PersonModel } from '../../models/person.model';
-import app from '../../index';
+import { PersonModel } from '../../models/person.model';
+import { EncounterModel } from 'src/models/encounter.model';
+import app from '../../server';
+import httpStatus from "http-status";
 
 const supertest = require('supertest');
 
@@ -134,6 +134,13 @@ const encounter7Data = {
     location: 'Auckland',
 }
 
+const encounterData: EncounterModel = {
+    date: new Date("2022-12-02"),
+    location: "here",
+    description: "we did this and that",
+    persons: [] as any,
+}
+
 describe('POST /encounter', () => {
     it('Successfully creating an encounter with empty persons array', async () => {
         await supertest(app).post('/api/encounters')
@@ -214,3 +221,45 @@ describe('POST /encounter', () => {
     })
 
 })
+
+describe('encounter ', () => {
+  it('is updated correctly', async () => {
+    // create an encounter
+    const newEncounter = await supertest(app).post('/api/encounters').set('Accept', 'application/json').send(encounterData)
+    const newEncounterId = newEncounter._body._id
+    expect(newEncounter._body.location).toBe(encounterData.location)
+
+    // change data
+    const changedLocation = "actually it's not there"
+    encounterData.location = changedLocation
+
+    // update an encounter
+    await supertest(app)
+        .put(`/api/encounters/${newEncounterId}`)
+        .set('Accept', 'application/json')
+        .send(encounterData)
+        .expect(httpStatus.NO_CONTENT)
+
+    // retrieve encounter from database, and check that the updated encounter contains the changed location
+    // TODO: require findEncounter service to be implemented (blocked by GET /encounters/:id)
+
+  });
+
+  it('is updated with invalid encounter object ID (non-castable)', async () => {
+    // update an encounter of id that does not exist
+    await supertest(app)
+        .put(`/api/encounters/${123}`)
+        .set('Accept', 'application/json')
+        .send(encounterData)
+        .expect(httpStatus.BAD_REQUEST)
+  });
+
+  it('is updated with encounter object ID that does not exist', async () => {
+    // update an encounter of id that does not exist
+    await supertest(app)
+        .put(`/api/encounters/622b36166bb3a4e3a1ef62f1`)
+        .set('Accept', 'application/json')
+        .send(encounterData)
+        .expect(httpStatus.NOT_FOUND)
+  });
+});
