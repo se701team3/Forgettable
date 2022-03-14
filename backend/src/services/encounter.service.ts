2d2aa1734e6ee14088a49816988494b6be4d6c24
Seong-June Her <sher812@aucklanduni.ac.nz>
2022-03-14 18:36:37 +1300

    Add comments in encounter service

--- backend/src/services/encounter.service.ts
+++ backend/src/services/encounter.service.ts
@@ -12,13 +12,15 @@ const updateEncounter = async (objectID: string, encounterDetails: EncounterMode
     return updatedEncounter;
 }
 
+// Service is used for deletePersons endpoint
 const deleteEncounterPerson = async (personID: string) => {
-  // Service is used for deletePersons endpoint
-
+  // Remove the person with the supplied ID from all existing encounters
   await Encounter.updateMany({ }, { $pullAll: {persons: [{ _id: personID}]} });
+  // Store references to the encounters that no longer contain persons so they can be removed from Users later
   const empty_encounters = await Encounter.find({persons : {$exists:true, $size:0}});
+  // Delete all the empty encounters
   await Encounter.deleteMany({persons: {$exists: true, $size: 0}});
-
+  
   // return encounters that may have empty persons fields
   return empty_encounters;
 }
