eeeae9b0c941363e851cf0aabe50bac82fdd1f5e
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-15 23:50:13 +1300

    Add auth to POST persons/

--- backend/src/controllers/person.controller.ts
+++ backend/src/controllers/person.controller.ts
@@ -18,12 +18,35 @@ export const createPerson: POST = async (
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
-  logger.info('POST /persons/create request from frontend');
+  logger.info('POST /persons request from frontend');
+  const auth_id = req.headers.authorization?.["user_id"];
 
   try {
-    const person = await personService.createPerson(req.body);
-    res.status(httpStatus.CREATED).send(person);
-  } catch (e) {
+    let user = await userService.getUserByAuthId(auth_id);
+
+    if (!user) {
+      res.status(httpStatus.NOT_FOUND).end();
+    } else {
+      // Create a new person with the provided information
+      const createdPerson = await personService.createPerson(req.body);
+
+      if (!createdPerson) {
+        res.status(httpStatus.BAD_REQUEST).end();
+      } else {
+        // Add a reference to the created person to the user
+        user = await userService.addPersonId(user.auth_id, createdPerson._id)
+        // If user doesn't contain reference to new person 
+        if (!user?.persons.includes(new mongoose.Types.ObjectId(createdPerson._id))) {
+          // return Conflict and delete person from db
+          await personService.deletePersons(createdPerson._id.toString());
+          res.status(httpStatus.CONFLICT).end();
+        } else {
+          res.status(httpStatus.CREATED).json(createdPerson).end();
+        }
+      }
+    }
+  }
+  catch (e) {
     next(e);
   }
 };
