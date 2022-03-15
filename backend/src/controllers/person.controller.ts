c6467f840bcd9308782be0edf45213853632e228
Seong-June Her <61758893+sher812@users.noreply.github.com>
2022-03-15 22:54:24 +1300

    Delete persons endpoint (#137)
    * Add DELETE Persons endpoint
    
    * Added delete services for deleteUserPerson and deleteUserEncounter
    
    * Add comments in encounter service

--- backend/src/controllers/person.controller.ts
+++ backend/src/controllers/person.controller.ts
@@ -8,8 +8,9 @@ import httpStatus from 'http-status';
 import { PersonModel } from 'src/models/person.model';
 import userService from '../services/user.service';
 import personService from '../services/person.service';
-import logger from '../utils/logger';
+import encounterService from '../services/encounter.service';
 
+import logger from '../utils/logger';
 import { POST } from './controller.types';
 
 export const createPerson: POST = async (
@@ -77,6 +78,47 @@ export const getAllPeople = async (
   }
 };
 
+export const deletePersons = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  ): Promise<void> => {
+    logger.info("DELETE /persons/:personID request from frontend");
+    const auth_id = req.headers.authorization?.["user_id"];
+    
+    const current_user = await userService.getUserByAuthId(auth_id);
+    const user_id = req.params.id;
+
+    const user_persons = current_user?.persons;
+    let string_persons = user_persons?.map(x => x.toString());
+
+    if (string_persons?.includes(user_id.toString())) {
+      try {
+        // Delete user from database
+        await personService.deletePersons(req.params.id);
+
+        // return encounters that may have empty persons fields
+        const empty_encounters = await encounterService.deleteEncounterPerson(req.params.id);
+        await userService.deleteUserPerson(req.params.id);
+
+        // Make sure that empty encounters are also deleted from User
+        for (let i = 0; i < empty_encounters.length; i++) {
+          await userService.deleteUserEncounter(empty_encounters[i]?._id.toString());
+        }
+        
+        // Notify frontend that the operation was successful
+        res.sendStatus(httpStatus.OK).end();
+      } catch(e) {
+  
+        next(e);
+      }
+    } else {
+      res.sendStatus(httpStatus.NOT_FOUND).end();
+    }
+    
+    res.status(httpStatus.OK).end();
+  };
+
 export const updatePersonWithId = async (
   req: Request,
   res: Response,
@@ -106,3 +148,4 @@ export const updatePersonWithId = async (
     next(error);
   }
 };
+
