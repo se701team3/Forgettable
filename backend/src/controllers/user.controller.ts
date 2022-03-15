ad5dbe6487755a00e75fd3d55b8f49fc43a9ddde
Joel <joelkahuhutch@gmail.com>
2022-03-15 15:54:18 +1300

    style: lint fix

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,17 +1,18 @@
+/* eslint-disable dot-notation */
+/* eslint-disable no-underscore-dangle */
 /**
  * Controller contains high-level operations using services, consumed by routes
  */
 import { NextFunction, Request, Response } from 'express';
 
-import userService from '../services/user.service';
 import httpStatus from 'http-status';
+import userService from '../services/user.service';
 
 export const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
-  
   let decodedToken: any;
   if (req.headers.authorization) {
     decodedToken = req.headers.authorization as any;
@@ -27,11 +28,10 @@ export const createUser = async (
       first_name: createdUser.first_name,
       last_name: createdUser.last_name,
       persons: createdUser.persons,
-      encounters: createdUser.encounters
+      encounters: createdUser.encounters,
     });
-
   } catch (e) {
-    if (e.name === "Conflict") {
+    if (e.name === 'Conflict') {
       res.status(httpStatus.CONFLICT).end();
     }
     next(e);
@@ -44,8 +44,8 @@ export const getUser = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    const auth_id = req.headers.authorization?.["user_id"];
-    const user = await userService.getUserByAuthId(auth_id);
+    const authId = req.headers.authorization?.['user_id'];
+    const user = await userService.getUserByAuthId(authId);
 
     if (!user) {
       res.status(httpStatus.NOT_FOUND).end();
@@ -56,10 +56,9 @@ export const getUser = async (
         first_name: user.first_name,
         last_name: user.last_name,
         persons: user.persons,
-        encounters: user.encounters
+        encounters: user.encounters,
       });
     }
-  
   } catch (e) {
     next(e);
   }
