685502914228f6ecef8d68aa77ef917065b07847
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-15 15:51:47 +1300

    create person page files

---
+++ forgettable-frontend/src/pages/PersonPage/PersonPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './PersonPage.module.css';
+
+const PersonPage = (props) => {
+  return (
+    <div className={classes.PersonPage}></div>
+  );
+};
+
+export default PersonPage;
