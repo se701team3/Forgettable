c6b5833c54275a873d2abc1df009594a090c04f9
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-16 02:52:39 +1300

    finalise API function call functions

--- forgettable-frontend/src/services/index.js
+++ forgettable-frontend/src/services/index.js
@@ -1,26 +1,76 @@
-import {getData, postData, putData} from './functions/helpers';
+import {getData, postData, putData, deleteData} from './functions/helpers';
 
-import axios from 'axios';
-
-const TOKEN = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImYxZDU2YTI1MWU0ZGRhM2Y0NWM0MWZkNWQ0ZGEwMWQyYjlkNzJlMGQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZm9yZ2V0dGFibGUtNzhiOTYiLCJhdWQiOiJmb3JnZXR0YWJsZS03OGI5NiIsImF1dGhfdGltZSI6MTY0NzM0Njg3OSwidXNlcl9pZCI6IlJnUFNjSnlqZWFic3ZBT3dnMEdJS2pTa1g0NjIiLCJzdWIiOiJSZ1BTY0p5amVhYnN2QU93ZzBHSUtqU2tYNDYyIiwiaWF0IjoxNjQ3MzQ2ODc5LCJleHAiOjE2NDczNTA0NzksImVtYWlsIjoiYWRhbS5hLndpZW5lckBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYWRhbS5hLndpZW5lckBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.VF5ink9jNcy4whXW-Fj4BmeAg0uLBWwYlAA9nJHgXM4XomwCGxhZy0KdfB6JtZvU9zlnyTc-4qgKTH0xmMLWv9-o-aX6QsojyBuZxT5hSIIi7h5QWbBYOX-T2Og0AegQ4UzSqx2z3-xcJDkD-GXE6cSiupD-Xi2XmkRvAEVns1E0cHVvqUvEQDAV1Bcq6mh4CNy-R3295EPNs6O0m3EO5XhTn0YfCpvLKzpJ04F7ApitgeXnbADgsEQJPaDkyJ1Q3XoVmQeXFXbfqdwRB3orvM80B7uqsKDozce17W5oYF6o1j2xQwORw2XX_fTU88U9WWI-U_uouwEqRB3e7VMQJA';
-
-
+/**
+ * Fetches data of a single Person, given the Person's id
+ * @param {string} id id of the person to retrieve
+ * @return {Promise} data of the person with the id
+ */
 export const getPerson = async (id) => {
   return await getData('persons/' + id);
 };
 
+/**
+ * Fetches data of all Persons
+ * @return {Promise} data of all persons
+ */
 export const getAllPersons = async () => {
   return await getData('persons');
 };
 
+/**
+ * Fetches data of the user given their id
+ * @return {Promise} data of the user using the system
+ */
 export const getUser = async () => {
   return await getData('users');
 };
 
+/**
+ * Creates a Person in the database given the Person's data
+ * @param {Object} person data of the person to create.
+ * Must include a first_name property.
+ * @return {Promise} data entry of the new Person created
+ */
 export const createPerson = async (person) => {
   return await postData('persons', person);
 };
 
+/**
+ * Updates a Person in the database given the Person's new data.
+ * New data will overwrite all old data.
+ * @param {string} id id of the Person to update
+ * @param {Object} person data of the Person to update.
+ * @return {Promise} data entry of the updated Person
+ */
 export const updatePerson = async (id, person) => {
   return await putData('persons/' + id, person);
+};
+
+/**
+ * Deletes a Person from the database given the Person's id
+ * @param {string} id id of the Person to delete
+ * @return {Promise}
+ */
+export const deletePerson = async (id) => {
+  return await deleteData('persons/' + id);
+};
+
+export const getEncounter = async (id) => {
+  return await getData('encounters/' + id);
+};
+
+export const getAllEncounters = async () => {
+  return await getData('encounters');
+};
+
+export const createEnncounter = async (encounter) => {
+  return await postData('encounters', encounter);
+};
+
+export const updateEncounter = async (id, encounter) => {
+  return await putData('encounters/' + id, encounter);
+};
+
+export const deleteEncounter = async (id) => {
+  return await deleteData('encounters/' + id);
 };
