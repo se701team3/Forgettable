5287e9f92551db14e3f1f86a4a4f6f9cac3a9663
rainasong <rainasong0509@gmail.com>
2022-03-17 00:13:18 +1300

    Add getEncounters api calls

--- forgettable-frontend/src/pages/encounters/encounters.js
+++ forgettable-frontend/src/pages/encounters/encounters.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import EncounterCard from '../../components/EncounterCard/EncounterCard';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import classes from './encounters.module.css';
 import IconButton from '../../components/IconButton/IconButton';
 import {getDateString} from '../../functions/dateFormatter';
-import PersonDrawer from '../../components/PersonDrawer/PersonDrawer';
 import EncounterDrawer from '../../components/EncounterDrawer/EncounterDrawer';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import EncounterDetailsModal from '../../components/EncounterDetailsModal/EncounterDetailsModal';
+import {deleteEncounter, getAllEncounters, getPerson} from '../../services';
 
 export default function Encounters() {
   const [isHover, setIsHover] = useState(false);
@@ -18,45 +18,9 @@ export default function Encounters() {
   const [selectedEncounter, setSelectedEncounter] = useState(undefined);
   const [modalOpen, setModalOpen] = useState(false);
 
-  const persons = [
-    {
-      first_name: 'Kent',
-      last_name: 'Dodds',
-      img: 'https://images.unsplash.com/photo-1646936218155-caedb4ed9bf7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-    },
-    {
-      first_name: 'Jed',
-      last_name: 'Watson',
-      img: 'https://images.unsplash.com/photo-1600010437709-10176a56ea45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
-    },
-    {
-      first_name: 'Marley',
-      last_name: 'George',
-      img: 'https://images.unsplash.com/photo-1646936218590-91d949706cb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    },
-    {
-      first_name: 'Jack',
-      last_name: 'Winston',
-    },
-    {
-      first_name: 'John',
-      last_name: 'Doe',
-    },
-  ];
+  const [encounterList, setEncounterList] = useState([]);
 
-  const [encounterList, setEncounterList] = useState(
-      [{
-        id: '0',
-        date: getDateString(new Date()),
-        location: 'Auckland',
-        title: 'Fermentum pellentesque',
-        description: 'Diam dictum vestibulum mi nulla vestibulum, id nibh. Nunc consequat amet commodo turpis tellus. Scelerisque a pellentesque vel accumsan sed mauris, ac turpis pharetra. Sem tristique nulla cursus praesent tincidunt integer',
-        persons: persons,
-      },
-      ],
-  );
-
-  const onClick = (encounter) => {
+  const handleCardClick = (encounter) => {
     console.log('card click');
     console.log(encounter);
     setSelectedEncounter(encounter);
@@ -67,38 +31,33 @@ export default function Encounters() {
     setModalOpen(false);
   };
 
-  const onDelete = () => {
-    console.log('delete');
+  const handleNewEntryClick = () => {
+    console.log('new entry');
+    // todo: to be implemented after create encounter page is created
   };
 
-  for (let i = 1; i < 20; i++) {
-    encounterList[i] = {
-      ...encounterList[0],
-      id: i.toString(),
-    };
-  }
+  const onDelete = (encounterId) => {
+    console.log('delete encounter id: ', encounterId);
+    // todo: to be implemented after backend delete encounters endpoint is merged
+    // const removeEncounter = async () => {
+    //   await deleteEncounter(encounterId).then((res) => {
+    //     console.log(res);
+    //   });
+    // };
+    // removeEncounter();
+  };
 
   const fetchMoreData = () => {
-    // console.log('fetch more');
-    if (encounterList.length > 100) {
-      setHasMore(false);
-      return;
-    }
+    console.log('fetch more');
+    // todo: to be implemented after backend paging feature is implemented
+    // if () {
+    //   setHasMore(false);
+    //   return;
+    // }
 
-    setTimeout(() => {
-      const newList = [];
-      newList[0] = encounterList[0];
-      for (let i = 0; i < 20; i++) {
-        newList[i] = {
-          ...newList[0],
-          id: i.toString(),
-        };
-      }
-      setEncounterList([...encounterList, ...newList]);
-    }, 1000);
+    // setTimeout(() => {
+    // }, 1000);
   };
-
-  // console.log(encounterList);
 
 
   const handleOnMouseOver = (index) => {
@@ -109,6 +68,29 @@ export default function Encounters() {
   const handleOnMouseOut = () => {
     setIsHover(false);
   };
+
+  const exportSearchString = (wordEntered) => {
+    // todo: to be implemented after backend search filter is implemented
+    // console.log(wordEntered);
+  };
+
+  useEffect(() => {
+    const getEncounterList = async () => {
+      await Promise.all(getAllEncounters().then((res) => {
+        res.length > 0 && res.map((r) => {
+          const persons = [];
+          r.persons.map(async (personId, index) => {
+            await getPerson(personId).then((person) => {
+              persons[index] = person;
+            });
+          });
+          r.persons = persons;
+        });
+        setEncounterList(res);
+      }));
+    };
+    getEncounterList();
+  }, []);
 
   return (
     <>
@@ -125,20 +107,20 @@ export default function Encounters() {
         open={modalOpen}
         onClose={handleModalClose}
         encounter={selectedEncounter}
-        onDelete={onDelete}
+        onDelete={() => onDelete(selectedEncounter._id)}
       />}
       <div className={classes.Container}>
         <div className={classes.Header}>
           Encounters
         </div>
         <div className={classes.Utilities}>
-          <SearchBar placeholder={'Search'}/>
+          <SearchBar hasAutocomplete={false} exportSearchString={exportSearchString} placeholder={'Search'}/>
           <div className={classes.Button}>
-            <IconButton btnText="New Entry" onClick={onClick} includeIcon={true} />
+            <IconButton btnText="New Entry" onClick={handleNewEntryClick} includeIcon={true} />
           </div>
         </div>
         <div className={classes.List}>
-          <InfiniteScroll
+          {encounterList.length > 0 ? <InfiniteScroll
             dataLength={encounterList.length}
             next={fetchMoreData}
             hasMore={hasMore}
@@ -153,21 +135,21 @@ export default function Encounters() {
               return (
                 <div key={`${index}-container`} onMouseOver={() => handleOnMouseOver(index)} onMouseOut={handleOnMouseOut}>
                   <EncounterCard
-                    key={encounter.id}
+                    key={encounter._id}
                     title={encounter.title}
                     description={encounter.description}
                     location={encounter.location}
                     persons={encounter.persons}
                     date={encounter.date}
                     className={classes.EncounterCard}
-                    onClick={() => onClick(encounter)}
-                    onDelete={onDelete}
+                    onClick={() => handleCardClick(encounter)}
+                    onDelete={() => onDelete(encounter._id)}
                     isInitialEncounter={false}
                   />
                 </div>
               );
             })}
-          </InfiniteScroll>
+          </InfiniteScroll> : <div><h3>No Encounters Found :(</h3></div>}
         </div>
       </div>
     </>
