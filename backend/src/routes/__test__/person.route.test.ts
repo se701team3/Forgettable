56db3bb9030be08e7e0afbb60a672c3e3582c825
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2022-03-16 16:39:27 +1300

    Add route tests for GET persons/:id

--- backend/src/routes/__test__/person.route.test.ts
+++ backend/src/routes/__test__/person.route.test.ts
@@ -5,6 +5,8 @@ import { PersonModel } from '../../models/person.model';
 import app from '../../server';
 import testUtils from '../../utils/test/test-utils';
 import "dotenv/config";
+import personService from '../../services/person.service';
+import userService from '../../services/user.service';
 
 const supertest = require('supertest');
 
@@ -24,21 +26,53 @@ const reqUserData = {
   persons: [] as any,
 }
 
-const person1Data:PersonModel = {
-  first_name: 'testFirstName',
-  last_name: 'testLastName',
-  interests: ['a', 'b'],
-  organisation: 'testorg',
+const person1Data: PersonModel = {
+  first_name: 'Ping',
+  last_name: 'Pong',
+  interests: ['video games', 'hockey'],
+  organisation: 'helloc',
   time_updated: new Date('2022-01-01'),
-  how_we_met: 'testmet',
+  how_we_met: 'Hockey club',
   birthday: new Date('2002-12-12'),
   encounters: [] as any,
   first_met: new Date('2022-01-01'),
-  gender: "other",
+  gender: "male",
+  location: "Auckland",
   image: null as any,
-  location: 'Someplace',
   social_media: null as any
 };
+
+const person2Data: PersonModel = {
+  first_name: 'Adam',
+  last_name: 'Bong',
+  interests: ['badminton', 'golf'],
+  organisation: 'helloc',
+  time_updated: new Date('2022-02-23'),
+  how_we_met: 'Skype',
+  birthday: new Date('2001-07-16'),
+  encounters: [] as any,
+  first_met: null as any,
+  gender: "male",
+  image: null as any,
+  location: null as any,
+  social_media: null as any
+}
+
+const person3Data: PersonModel = {
+  first_name: 'Billy',
+  last_name: 'John',
+  interests: ['surfing', 'cooking'],
+  organisation: 'an organisation',
+  time_updated: new Date('2022-02-23'),
+  how_we_met: 'At the park',
+  birthday: new Date('2001-07-16'),
+  encounters: [] as any,
+  first_met: null as any,
+  gender: "male",
+  image: null as any,
+  location: null as any,
+  social_media: null as any
+}
 
 describe('Creating a person', () => {
   it('Can be created and stored in the user', async () => {
@@ -96,5 +130,76 @@ describe('Creating a person', () => {
       .expect(httpStatus.OK);
 
     expect(updatedPerson.location).toEqual(changedLocation);
+  });
+});
+
+describe('Getting persons', () => {
+  it ('Can be retrieved by id', async () => {
+    // Create a new user
+    await supertest(app).post('/api/users')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .send(reqUserData);
+
+    // Create a new person and store it in the user
+    const { body: createdPerson } = await supertest(app).post('/api/persons')
+      .set('Accept', 'application/json')
+      .send(person1Data)
+      .set("Authorization", token)
+      .expect(httpStatus.CREATED);
+
+    // Attempt to retrieve it
+    const { body: retrievedPerson } = await supertest(app).get(`/api/persons/${createdPerson._id}`)
+      .set('Accept','application/json')
+      .set("Authorization", token)
+      .expect(httpStatus.OK);
+
+    expect(retrievedPerson._id).toEqual(createdPerson._id);
+  });
+
+  it ('Is not retrieved if the user does not contain it', async () => {
+    // Create a new user
+    const { body: user } = await supertest(app).post('/api/users')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .send(reqUserData);
+
+    // Create three persons and store only the first two in the user
+    const { body : person1 } = await supertest(app).post('/api/persons')
+      .set('Accept', 'application/json')
+      .send(person1Data)
+      .set("Authorization", token)
+      .expect(httpStatus.CREATED);
+
+    const { body : person2 } = await supertest(app).post('/api/persons')
+      .set('Accept', 'application/json')
+      .send(person2Data)
+      .set("Authorization", token)
+      .expect(httpStatus.CREATED);  
+
+    const person3 = await personService.createPerson(person3Data);
+
+    // Ensure only person1 and person2 can be retrieved
+    await supertest(app).get(`/api/persons/${person1._id}`)
+      .set('Accept','application/json')
+      .set("Authorization", token)
+      .expect(httpStatus.OK);
+
+    await supertest(app).get(`/api/persons/${person2._id}`)
+      .set('Accept','application/json')
+      .set("Authorization", token)
+      .expect(httpStatus.OK);
+
+    await supertest(app).get(`/api/persons/${person3._id}`)
+      .set('Accept','application/json')
+      .set("Authorization", token)
+      .expect(httpStatus.NOT_FOUND);
+  });
+
+  it ('Returns "Unauthorized" if the user does not have a valid auth_id', async () => {
+      await supertest(app).get(`/api/persons/FAKE_PERSON_ID`)
+        .set('Accept','application/json')
+        .set("Authorization", "FAKE_AUTH_TOKEN")
+        .expect(httpStatus.UNAUTHORIZED);
   });
 });
