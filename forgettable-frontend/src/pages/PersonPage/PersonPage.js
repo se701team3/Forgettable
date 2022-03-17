b509d3f7e3a6ce3fd9b80c454a20b2f34ec80a42
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-17 21:31:53 +1300

    render encounters dynamically

--- forgettable-frontend/src/pages/PersonPage/PersonPage.js
+++ forgettable-frontend/src/pages/PersonPage/PersonPage.js
@@ -4,53 +4,85 @@ import IconButton from '../../components/IconButton/IconButton';
 import PersonDrawer from '../../components/PersonDrawer/PersonDrawer';
 import {createPerson, getPerson} from '../../services';
 import classes from './PersonPage.module.css';
+import {ENCOUNTERS} from './PlaceholderData';
 
 const PersonPage = (props) => {
-  const personId = '6232a8af5ae047a37b7e575d';
+  const personId = '6232b2d25ae047a37b7e57b3';
 
-  const [person, setPerson] = useState();
+  const [person, setPerson] = useState({
+    firstName: '',
+    lastName: '',
+    birthday: null,
+    gender: '',
+    location: '',
+    howWeMet: '',
+    firstMet: null,
+    interests: [],
+    organisation: '',
+    socialMedia: [],
+    img: '',
+    encounters: [],
+    timeUpdated: null,
+  });
 
   useEffect(async () => {
     const result = await getPerson(personId);
+
     setPerson({
       firstName: result.first_name,
       lastName: result.last_name,
-      birthday,
-      gender,
-      location,
+      birthday: result.birthday,
+      gender: result.gender,
+      location: result.location,
       howWeMet: result.how_we_met,
-      interests,
-      organisation,
-      socialMedia: result.social_media,
+      interests: result.interests,
+      organisation: result.organisation,
+      // socialMedia: result.social_media,
       img: result.image,
-      encounters,
+      // encounters: result.encounters,
+      encounters: ENCOUNTERS,
       timeUpdated: result.timeUpdated,
     });
-
-    console.log(create);
   }, [personId]);
 
+  const createEncounterComponent = (encounter, i) => {
+    return (
+      <div className={classes.CardWrapper} key={encounter.id || i}>
+        <EncounterCard
+          title={encounter.title}
+          description={encounter.description}
+          persons={encounter.persons}
+          location={encounter.location}
+          onClick={() => {}}
+          onDelete={() => {}}
+          date={encounter.date}
+          isInitialEncounter={false}
+        />
+      </div>
+    );
+  };
 
   return (
     <div className={classes.PersonPage}>
       <PersonDrawer
         open={true}
-        img="https://user-images.githubusercontent.com/62003343/158300159-495244dd-f05e-4384-9fac-ddf88a06be39.gif"
+        img={person.img}
         staticDrawer={true}
-        name={'Mercury Lin'}
-        firstMet={new Date()}
-        interests={['art', 'sewing', 'coding']}
-        organisation="Team3"
-        gender="Female"
-        birthday={new Date('01-01-2010')}
-        socialMedias={[{name: 'facebook', link: 'link'}, {name: 'instagram', link: 'link'}]}
+        name={`${person.firstName} ${person.lastName}`}
+        firstMet={person.firstMet}
+        interests={person.interests}
+        organisation={person.organisation}
+        location={person.location}
+        gender={person.gender}
+        birthday={person.birthday}
+        socialMedias={person.socialMedia}
       />
       <div className={classes.ContentContainer}>
         <div className={classes.TitleContainer} >
           <h1 className={classes.Title}>
             You encountered
             <br/>
-            Mercury 4 times
+            {person.firstName} {person.encounters.length} times
           </h1>
           <div className={classes.ButtonContainer}>
             <IconButton
@@ -62,92 +94,11 @@ const PersonPage = (props) => {
           </div>
         </div>
         <div className={classes.EncountersContainer}>
-
-          <div className={classes.CardWrapper}>
-            <EncounterCard
-              title="Aenean et quam"
-              description="Nunc aliquet leo vitae sapien varius ullamcorper. Donec eget vestibulum turpis. Suspendisse dolor urna, ultricies in neque quis, dictum sodales arcu. "
-              persons={[
-                {
-                  first_name: 'Mercury',
-                  last_name: 'Lin',
-                  img: 'https://user-images.githubusercontent.com/62003343/158300159-495244dd-f05e-4384-9fac-ddf88a06be39.gif',
-                },
-              ]}
-              location='Auckland'
-              onClick={() => {}}
-              onDelete={() => {}}
-              date={new Date()}
-              isInitialEncounter={false}
-            />
-          </div>
-          <div className={classes.CardWrapper}>
-            <EncounterCard
-              title="Sed a est venenatis"
-              description="Sed sed urna dui. Nam gravida, leo ac molestie pulvinar, odio erat egestas lorem, in ullamcorper lacus eros eu purus. Proin nulla neque, tristique ac euismod ut, convallis vitae mi. "
-              persons={[
-                {
-                  first_name: 'Mercury',
-                  last_name: 'Lin',
-                  img: 'https://user-images.githubusercontent.com/62003343/158300159-495244dd-f05e-4384-9fac-ddf88a06be39.gif',
-                },
-                {
-                  first_name: 'Raina',
-                  last_name: 'Song',
-                  img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-                },
-                {
-                  first_name: 'Hazel',
-                  last_name: 'Williams',
-                  img: 'https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png',
-                },
-              ]}
-              location='Auckland'
-              onClick={() => {}}
-              date={new Date()}
-              onDelete={() => {}}
-              isInitialEncounter={false}
-            />
-          </div>
-
-          <div className={classes.CardWrapper}>
-            <EncounterCard
-              title="Good times!"
-              description="Diam dictum vestibulum mi nulla vestibulum, id nibh. Nunc consequat amet commodo turpis tellus. Scelerisque a pellentesque vel accumsan sed mauris, ac turpis pharetra. Sem tristique nulla cursus praesent tincidunt integer faucibus in aliquam. Pretium nam enim ut suspendisse dictum. Arcu tristique turpis nam nunc nisl. Mi, molestie quis tincidunt ipsum hendrerit. Lacinia at ut dui nibh placerat scelerisque congue egestas ut. Et fusce odio amet, etiam consectetur ipsum a. Adipiscing iaculis dictum nisl lobortis. Pharetra nunc semper adipiscing massa aenean eu iaculis risus. At quam feugiat lorem ac dui. A, facilisi lectus mus iaculis convallis proin semper dolor accumsan. Ut nibh quis porttitor faucibus sagittis netus sit quis."
-              persons={[
-                {
-                  first_name: 'Mercury',
-                  last_name: 'Lin',
-                  img: 'https://user-images.githubusercontent.com/62003343/158300159-495244dd-f05e-4384-9fac-ddf88a06be39.gif',
-
-                },
-              ]}
-              location='Auckland'
-              onClick={() => {}}
-              onDelete={() => {}}
-              isInitialEncounter={false}
-            />
-          </div>
-
-          <div className={classes.CardWrapper}>
-            <EncounterCard
-              title="Good times!"
-              description="Diam dictum vestibulum mi nulla vestibulum, id nibh. Nunc consequat amet commodo turpis tellus. Scelerisque a pellentesque vel accumsan sed mauris, ac turpis pharetra. Sem tristique nulla cursus praesent tincidunt integer ..."
-              persons={[
-                {
-                  first_name: 'Mercury',
-                  last_name: 'Lin',
-                  img: 'https://user-images.githubusercontent.com/62003343/158300159-495244dd-f05e-4384-9fac-ddf88a06be39.gif',
-
-                },
-              ]}
-              location='Auckland'
-              onClick={() => {}}
-              onDelete={() => {}}
-              isInitialEncounter={true}
-              date={new Date()}
-            />
-          </div>
+          {
+            person.encounters.map((encounter, i) => {
+              return createEncounterComponent(encounter, i);
+            })
+          }
         </div>
       </div>
     </div>
