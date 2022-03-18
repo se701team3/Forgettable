731546c29783a5500c01081b460da7d3556a4350
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-03-18 17:58:31 +1300

    feat delete confirmation modal added the confimation modal for the delete button. Will also now redirect to the persons page.

--- forgettable-frontend/src/pages/edit/EditPerson.js
+++ forgettable-frontend/src/pages/edit/EditPerson.js
@@ -22,26 +22,6 @@ export default function EditPerson() {
 
   const {id} = (!create) && useParams();
 
-  // let personData = {};
-
-  // temp data
-  // (!create) && (
-  //   personData = {
-  //     first_name: 'Name',
-  //     last_name: 'Last',
-  //     birthday: '2012-03-04',
-  //     gender: 'male',
-  //     location: 'here',
-  //     first_met: '2001-01-01',
-  //     how_we_met: 'idk',
-  //     interests: 'a',
-  //     organisation: 'job co.',
-  //     social_media: new Map([['twitter', 'fdfd']]),
-  //     image: 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAI2SURBVHhe7dpBcgFBFIdxspqlJTtu4hhuwC0cgx23MMexdAPLyavqri6FZOYfyWuv8v0WySBJma9aT6cZd103wjAf+TsGIJaAWAJiCYglIJaAWAJiCYglIJaAWAJiCYglIJaAWAJiCYglIJaAWAJiCYglIJaAWAJiCf5LrLZtF4vF+IHdeTwe8w/16iI7nU7L5fJwOOTbN+yh+XyeT/JbTdPk3+kTO9Z0OrWztQGSTvtnNptN/nN9gsUaPl6eGt7lqQCxXgn0Yp077xtLavS7Ub7yprGs1GQyySUelDT5ttdl6k1jPY6pp2MnP+YV603XWev1Oh2URrvdLt1TUexP/pVFg89Z8O+OgFgCYgmIJQgcq23bfOQl8NVwNptdLpd0zNWwRylla7F08NcCjyznRZZhghdEjeU/u5uoL0P/2d1EHVn+s7uJOrL8Z3fDBC8IGWu/3+cjZzaMw0nvgJnVapXvchFyzioT1vV6bZomHTuIHcv5ycebszyXo/efkAg3sjyXo1bqfD7nGxFHludytLzJlMQbWfZqSAf+z5xFqSBYrCqbDUWwl2GVzYYi2MiqstlQBBtZFWd3wwQvIJaAWAJiCSLFqrbnV9hlJYpae35FpKVDWTc47/kVIWPVes5M8AJiCYgliBSryqR+K1Ks7XZrX6vsNyTxtpUrYs4SEEtALAGxBMQSEEtALAGxBMQSEEtALAGxBMQSEEtALAGxBMQSEEtALAGxBMQSEEtALAGxBhuNPgHlJKV46HCjogAAAABJRU5ErkJggg==',
-  //     encounters: [],
-  //     time_updated: '0002-02-02',
-  //   });
-
   const [personData, setPersonData] = useState({
     first_name: 'Name',
     last_name: 'Last',
@@ -58,18 +38,35 @@ export default function EditPerson() {
     time_updated: '0002-02-02',
   });
 
+  // const [personData, setPersonData] = useState({
+  //   first_name: '',
+  //   last_name: '',
+  //   birthday: null,
+  //   gender: '',
+  //   location: '',
+  //   first_met: null,
+  //   how_we_met: '',
+  //   interests: '',
+  //   organisation: '',
+  //   social_media: new Map(),
+  //   image: '',
+  //   encounters: [],
+  //   time_updated: '',
+  // });
+
   function handleUpdateData(newData) {
     setPersonData(newData);
   }
 
-  useEffect(async () => {
-    try {
-      receivedData = await apiCalls.getPerson(id);
-      handleUpdateData(receivedData);
-    } catch (err) {
-      console.log(err);
-    }
-  }, [id]);
+  (!create) && (
+    useEffect(async () => {
+      try {
+        receivedData = await apiCalls.getPerson(id);
+        handleUpdateData(receivedData);
+      } catch (err) {
+        console.log(err);
+      }
+    }, [id]));
 
   let initialProfilePic = '';
   let initialProfilePicPreview = '';
@@ -91,6 +88,7 @@ export default function EditPerson() {
   const [currentSocialMedia, setCurrentSocialMedia] = useState();
 
   const invisSocialMediaSubmitRef = useRef(null);
+  const invisDeleteSubmitRef = useRef(null);
 
   function handleSocialMediaModalOpen(key) {
     key ? setCurrentSocialMedia(key) : setCurrentSocialMedia();
@@ -141,6 +139,16 @@ export default function EditPerson() {
     }
   }
 
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  function handleDeleteModalOpen() {
+    setDeleteModalOpen(true);
+  };
+
+  const handleDeleteModalClose = () => {
+    setDeleteModalOpen(false);
+  };
+
   function handleDeletePerson() {
     async function deletePersonCall() {
       try {
@@ -150,6 +158,7 @@ export default function EditPerson() {
       }
     }
     deletePersonCall();
+    navigate('/people', {'replace': true});
   }
 
   function handleSubmit(event) {
@@ -231,7 +240,7 @@ export default function EditPerson() {
         <div className={classes.formButtonsDiv}>
           {!create && (
             <CustomButton btnText="Delete"
-              onClick={() => handleDeletePerson()}
+              onClick={() => handleDeleteModalOpen()}
               className={classes.formsButton}
             />
           )}
@@ -244,7 +253,7 @@ export default function EditPerson() {
           />
           <label htmlFor='submit'>
             <CustomButton btnText="Save"
-              // onClick={() => navigate('/encounters/create', {'replace': true})}
+              onClick={() => navigate('/encounters/create', {'replace': true})}
             />
           </label>
           <input ref={invisSocialMediaSubmitRef} id='submit' type="submit" className={classes.hiddenSubmit}/>
@@ -264,6 +273,23 @@ export default function EditPerson() {
               <InputField inputID='platform' inputLabel='Platform: ' inputType='secondary' inputStateValue={currentSocialMedia}/>
               <InputField inputID='url_link' inputLabel='URL Link: ' inputType='secondary' inputStateValue={socialMedias.get(currentSocialMedia)}/>
               <input ref={invisSocialMediaSubmitRef} type='submit' className={classes.hiddenSubmit}></input>
+            </div>
+          </form>
+        </div>
+      </CustomModal>
+
+      <CustomModal
+        open={deleteModalOpen}
+        onClose={handleDeleteModalClose}
+        hasCancel
+        hasConfirm
+        onConfirm={() => (invisDeleteSubmitRef.current.click())} >
+        <div className={classes.deleteModalDiv}>
+          <div className={classes.delteModalTitle}>Warning</div>
+          <form onSubmit={handleDeletePerson}>
+            <div className={classes.deleteModalMessageDiv}>
+              Are you sure you want to delete this person? You cannot undo this action.
+              <input ref={invisDeleteSubmitRef} type='submit' className={classes.hiddenSubmit}></input>
             </div>
           </form>
         </div>
