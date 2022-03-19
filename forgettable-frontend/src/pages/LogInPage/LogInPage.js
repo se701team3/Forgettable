42edded7c14d2d14800baed5e89b690976445dc3
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-19 22:09:34 +1300

    introduce auth context to log in page

--- forgettable-frontend/src/pages/LogInPage/LogInPage.js
+++ forgettable-frontend/src/pages/LogInPage/LogInPage.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from './LogInPage.module.css';
 import logo from '../../assets/logos/logo-black.svg';
 import googleLogo from '../../assets/icons/google-login.svg';
+import {AuthContext} from '../../context/AuthContext';
 
 const LogInPage = (props) => {
-  return (
+  const authContext = useContext(AuthContext);
 
+  return (
     <div className={classes.LogInPage}>
       <div className={classes.ContentContainer}>
-
         <div className={classes.ContentWrapper}>
           <p className={classes.WelcomeBackText}>
             Welcome Back!
           </p>
-
-
-          <div
-            className={classes.SignInButton} onClick={() => {}}>
+          <button
+            className={classes.SignInButton} onClick={authContext.login}>
             <img src={googleLogo} alt="google logo" />
             <p>Sign in with Google</p>
-          </div>
-
-
+          </button>
           <p className={classes.CreateAccountText}>
             Please sign in using your Google account, if you do not have an
-            account, create one <a href="https://accounts.google.com/SignUp?hl=en">here</a>
+            account, create one
+            <a href="https://accounts.google.com/SignUp?hl=en">here</a>
           </p>
         </div>
-
         <img
           className={classes.Logo}
           src={logo}
