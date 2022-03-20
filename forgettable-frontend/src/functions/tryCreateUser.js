abf9540e179dd3af03f5371fbd79f2cfa467aff9
Mercury Lin <62003343+lin8231@users.noreply.github.com>
2022-03-20 15:46:50 +1300

    fix create uesr (#292)

--- forgettable-frontend/src/functions/tryCreateUser.js
+++ forgettable-frontend/src/functions/tryCreateUser.js
@@ -1,6 +1,11 @@
+import {createUser} from '../services';
+
 export const tryCreateUser = async (userObj) => {
   try {
-    const result = await createUser(userObj);
+    const result = await createUser({
+      first_name: userObj.userName.split(' ')[0],
+      last_name: userObj.userName.split(' ')[1] || '',
+    });
 
     if (result) {
       console.log('Successfully made user');
