1346ea9e7f75e12337a1b06c9c9f26ae0176ac38
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-20 17:35:46 +1300

    move browser router into index.js

--- forgettable-frontend/src/App.js
+++ forgettable-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './index.css';
 import {
-  BrowserRouter,
+  BrowserRouter, useNavigate,
 } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import PageRouter from './hoc/PageRouter/PageRouter';
@@ -9,13 +9,19 @@ import {AuthContext} from './context/AuthContext';
 import auth from './services/auth';
 import firebase from 'firebase/compat/app';
 import {tryCreateUser} from './functions/tryCreateUser';
+import {useLocation} from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [user, setUserInfo] = useState({});
 
   useEffect(() => {
+    const currentLocation = location.pathname;
+    console.log('Current location: ', currentLocation);
+
     const status = auth.loadLoginStatus();
 
     if (status) setIsLoggingIn(true);
@@ -27,10 +33,15 @@ function App() {
         setLoggedIn(false);
       }
       setIsLoggingIn(false);
+      console.log('navigating to: ', currentLocation);
+      navigate(currentLocation);
     });
   }, []);
 
   const login = () => {
+    const currentLocation = location.pathname;
+    console.log('Current location: ', currentLocation);
+
     setIsLoggingIn(true);
 
     auth.signIn(async (ok, user) => {
@@ -46,6 +57,8 @@ function App() {
         setIsLoggingIn(false);
         console.log('Error logging in');
       }
+      console.log('navigating to: ', currentLocation);
+      navigate(currentLocation);
     });
   };
 
@@ -59,15 +72,12 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <AuthContext.Provider
-        value={{isLoggedIn, isLoggingIn, user, login, logout}}
-      >
-        {isLoggedIn && <NavBar />}
-        <PageRouter/>
-
-      </AuthContext.Provider>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{isLoggedIn, isLoggingIn, user, login, logout}}
+    >
+      {isLoggedIn && <NavBar />}
+      <PageRouter/>
+    </AuthContext.Provider>
   );
 }
 
