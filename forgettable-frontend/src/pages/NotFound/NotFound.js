27895d7b19b288a48d64b217db01d1119abbbc1a
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-20 17:45:29 +1300

    create not-found page

---
+++ forgettable-frontend/src/pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './NotFound.module.css';
+
+const NotFound = (props) => {
+  return (
+    <div className={classes.NotFound}>
+      <div className={classes.ContentContainer}>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default NotFound;
