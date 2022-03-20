6c82a794b1c05d6876fb8fb275d46602e569f02d
ZHENK <chengzhenyang@gmail.com>
2022-03-21 03:00:08 +1300

    Fix getImageSrcFromBuffer by displaying base64 images

--- forgettable-frontend/src/functions/getImageSrcFromBuffer.js
+++ forgettable-frontend/src/functions/getImageSrcFromBuffer.js
@@ -3,9 +3,9 @@ global.Buffer = global.Buffer || require('buffer').Buffer;
 /**
  * Converts a image buffer into a format that a <img> src can interpret
  * @param {*} buffer the buffer to be converted into an image src
- * @returns format that a <img> src can interpret
+ * @return format that a <img> src can interpret
  */
 export const getImageSrcFromBuffer = (buffer) => {
   if (!buffer) return null;
-  return Buffer.from(buffer, 'base64');
+  return 'data:image/png;base64, ' + Buffer.from(buffer, 'base64');
 };
