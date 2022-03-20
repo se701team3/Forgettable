aad2ab01c4e448953d46d85424692feaa4a510a0
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-20 19:42:37 +1300

    update settings page

--- forgettable-frontend/src/pages/SettingsPage/SettingsPage.js
+++ forgettable-frontend/src/pages/SettingsPage/SettingsPage.js
@@ -3,6 +3,7 @@ import classes from './SettingsPage.module.css';
 import {signOut} from 'firebase/auth';
 import DarkMode from './DarkMode';
 import {authentication} from '../../services/auth';
+import CustomButton from '../../components/CustomButton/CustomButton';
 
 function SettingsPage({setIsLoggedIn}) {
   const user = localStorage.getItem('userName');
@@ -30,10 +31,20 @@ function SettingsPage({setIsLoggedIn}) {
           <h4>Email</h4>
           <p>dsafsafsaS</p>
         </div>
-
+        <CustomButton
+          btnText="Log out"
+          className={classes.Button}
+          onClick={signOutHandler}
+        />
         <h2>
           Appearance
         </h2>
+        <div className={classes.DarkModeContainer}>
+          <p>Dark mode</p>
+          <div className={classes.TogglerContainer}>
+            <DarkMode/>
+          </div>
+        </div>
       </div>
     </div>
   );
