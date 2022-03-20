564f68714e49f32b96252d9fe1df93fcf7e17312
lin8231 <62003343+lin8231@users.noreply.github.com>
2022-03-20 19:13:01 +1300

    update settings page

--- forgettable-frontend/src/pages/SettingsPage/SettingsPage.js
+++ forgettable-frontend/src/pages/SettingsPage/SettingsPage.js
@@ -8,34 +8,17 @@ function SettingsPage({setIsLoggedIn}) {
   const user = localStorage.getItem('userName');
 
   const signOutHandler = ()=>{
-    signOut(authentication).then(() => {
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-      console.log(error);
+    signOut(authentication).catch((error) => {
+      console.log('Error signing out', error);
     });
-    localStorage.removeItem('token');
     localStorage.removeItem('userName');
     setIsLoggedIn(false);
   };
 
   return (
-    <div className='SettingsPage'>
-      <div className={classes.title}>Settings:</div>
-      <div className={classes.account}>Account:</div>
-      <div className={classes.border}></div>
-      <div className={classes.name}>Name</div>
-      <div className={classes.content}>{user ? user : 'user name'}</div>
-      {/* <div className={classes.email}>Email</div>
-      <div className={classes.content}>username@gmail.com</div> */}
-      <div className={classes.border2}></div>
-      <button className={classes.button} onClick={signOutHandler}>Log out</button>
-      <div className={classes.appearence}>Appearence:</div>
-      <div className={classes.border}></div>
-      <toolbar className={classes.toolbar}>
-        <div className={classes.darkMode}>Dark mode</div>
-        <DarkMode />
-      </toolbar>
+    <div className={classes.SettingsPage}>
+      <div className={classes.ContentContainer}>
+      </div>
     </div>
   );
 }
