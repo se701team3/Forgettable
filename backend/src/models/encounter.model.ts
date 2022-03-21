d519f2ba3006abc704101954ddfc4978fc469344
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-03-21 22:19:03 +1300

    bugfix - undo algolia changes

--- backend/src/models/encounter.model.ts
+++ backend/src/models/encounter.model.ts
@@ -1,5 +1,4 @@
 import mongoose, { Schema, model } from 'mongoose';
-const mongooseAlgolia = require('mongoose-algolia');
 
 export interface EncounterModel {
   title: string,
@@ -11,7 +10,7 @@ export interface EncounterModel {
 }
 
 const schema = new Schema<EncounterModel>({
-  title: { type: String, required: true},
+  title: { type: String, required: true },
   date: { type: Date, required: false },
   time_updated: { type: Date, default: new Date(Date.now()), required: true },
   location: { type: String, required: false },
@@ -19,24 +18,4 @@ const schema = new Schema<EncounterModel>({
   persons: { type: [mongoose.Types.ObjectId], required: true },
 });
 
-schema.plugin(mongooseAlgolia, {
-  appId: process.env.ALGOLIA_APP_ID,
-  apiKey: process.env.ALGOLIA_SECRET_KEY,
-  indexName: 'encounters',
-  selector: 'title date time_updated location description persons',
-  debug: true,
-});
-
-const encounterModel = model<EncounterModel>('Encounter', schema);
-
-(encounterModel as any).SyncToAlgolia();
-
-(encounterModel as any).SetAlgoliaSettings({
-  searchableAttributes: [
-    'title',
-    'location',
-    'description',
-  ],
-});
-
-export default encounterModel;
+export default model<EncounterModel>('Encounter', schema);
--- backend/src/services/encounter.service.ts
+++ backend/src/services/encounter.service.ts
@@ -3,13 +3,7 @@ import mongoose from 'mongoose';
 import Encounter, { EncounterModel } from '../models/encounter.model';
 import logger from '../utils/logger';
 
-const algoliaSearch = require('algoliasearch');
-
-const client = algoliaSearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_SECRET_KEY,
-);
-const index = client.initIndex('encounters');
+const queryKeys = ['title', 'location', 'description'];
 
 const createEncounter = async (encounterDetails: EncounterModel) => {
   const encounter = new Encounter(encounterDetails);
@@ -26,10 +20,9 @@ const createEncounter = async (encounterDetails: EncounterModel) => {
   return encounter;
 };
 
-const getEncounter = async (encounterId) => Encounter.findOne({_id: encounterId});
+const getEncounter = async (encounterId) => Encounter.findOne({ _id: encounterId });
 
 const getAllEncounters = async (queryParams: any, userEncounters: mongoose.Types.ObjectId[]) => {
-  
   // Get all encounters from the db which belongs to the user
   let foundUserEncounters = await Encounter.find({ _id: { $in: userEncounters } });
 
@@ -39,14 +32,19 @@ const getAllEncounters = async (queryParams: any, userEncounters: mongoose.Types
     logger.info(queryParams);
     const termValue = queryParams.term.toLowerCase();
 
-    const algoliaSearchResults = await index.search(termValue);
-    const algoliaSearchResultsIds = algoliaSearchResults.hits.map((hit) => hit.objectID.toString())
-
-    const userEncounterResults = foundUserEncounters.filter(
-      (encounter) => algoliaSearchResultsIds.includes(encounter._id.toString()),
-    );
-
-    foundUserEncounters = userEncounterResults;
+    // If no relevant fields in an Encounter match 'termValue', remove them from the array
+    foundUserEncounters = foundUserEncounters.filter((encounter) => {
+      for (let i = 0; i < queryKeys.length; i++) {
+        // Make sure person has a value for current queryKey
+        if (encounter[queryKeys[i]]) {
+          const encounterValue = (encounter[queryKeys[i]] as string).toLowerCase();
+          if (encounterValue.includes(termValue)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
   }
 
   return foundUserEncounters;
@@ -57,41 +55,33 @@ const updateEncounter = async (objectID: string, encounterDetails: EncounterMode
   const updatedEncounter = await Encounter
     .findByIdAndUpdate(objectID, encounterDetails, { new: true });
 
-  const updatedEncounterAlgolia : any = await Encounter.findById(objectID);
-  updatedEncounterAlgolia.objectID = objectID;
-  await index.partialUpdateObject(updatedEncounterAlgolia);
-
   return updatedEncounter;
 };
 
 // Service is used for deletePersons endpoint
 const deleteEncounterPerson = async (personID: string) => {
   // Remove the person with the supplied ID from all existing encounters
-  const result = await Encounter.updateMany({ }, { $pullAll: {persons: [{ _id: personID}]} });
+  const result = await Encounter.updateMany({ }, { $pullAll: { persons: [{ _id: personID }] } });
   // Store references to the encounters that no longer contain persons so they can be removed from Users later
-  const empty_encounters = await Encounter.find({persons : {$exists:true, $size:0}});
+  const empty_encounters = await Encounter.find({ persons: { $exists: true, $size: 0 } });
   // Delete all the empty encounters
-  const deleted_encounters = await Encounter.deleteMany({persons: {$exists: true, $size: 0}});
+  const deleted_encounters = await Encounter.deleteMany({ persons: { $exists: true, $size: 0 } });
 
-  // Check that all deleted encounters are returned and Encounters have been updated 
+  // Check that all deleted encounters are returned and Encounters have been updated
   if (empty_encounters.length == deleted_encounters.deletedCount) {
-    return {"array": empty_encounters, "bool": true};
-  } else {
-    return {"array": empty_encounters, "bool": false};
+    return { array: empty_encounters, bool: true };
   }
-}
+  return { array: empty_encounters, bool: false };
+};
 
 const deleteEncounter = async (encounterID: String) => {
   const result = await Encounter.deleteOne({ _id: encounterID }).exec();
-  
+
   // Check that Encounter has been deleted
   if (result.deletedCount == 1) {
-    await index.deleteObject(encounterID);
     return true;
-  } else {
-    return false;
   }
-
+  return false;
 };
 
 const encounterService = {
