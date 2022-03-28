a18fdb52e6990e65c6520be98657a80994db83a8
JL-2644 <67808129+JL-2644@users.noreply.github.com>
2022-03-28 14:06:02 +1300

    Update time_updated in encounter and person put endpoints (#329)

--- backend/src/controllers/encounter.controller.ts
+++ backend/src/controllers/encounter.controller.ts
@@ -103,6 +103,8 @@ export const updateEncounter = async (
 
     // update encounter
     const newEncounterData = getEncounterFromReqBody(req.body);
+    // update time_updated to the present time
+    newEncounterData.time_updated = new Date(Date.now());
     const updatedEncounter = await encounterService.updateEncounter(
       encounterIdToUpdate,
       newEncounterData,
