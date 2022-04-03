21c13d9e729bcd76f625ba1a0c9f03b91a888f2f
Ajay <abis555@aucklanduni.ac.nz>
2022-04-03 18:46:57 +1200

    [#220] - Combine first_name and last_name into name field

--- forgettable-frontend/src/components/UpcomingBirthdaySummary/UpcomingBirthdaySummary.js
+++ forgettable-frontend/src/components/UpcomingBirthdaySummary/UpcomingBirthdaySummary.js
@@ -23,11 +23,7 @@ const UpcomingBirthdaySummary = (props) => {
       <div className={classes.ContentContainer}>
         <div className={classes.HeaderContainer}>
           <Avatar
-<<<<<<< HEAD
             alt={props.name}
-=======
-            alt={props.firstName}
->>>>>>> 032f914974408b1a7dd5cb7910b4e231deabc3c5
             src={getImageSrcFromBuffer(props.img)}
             sx={{
               height: '70px',
@@ -41,11 +37,7 @@ const UpcomingBirthdaySummary = (props) => {
             }}
           />
           <div className={classes.IdentityInfoConatiner}>
-<<<<<<< HEAD
             <h3 data-testid="name-element">{props.name}</h3>
-=======
-            <h3 data-testid="name-element">{props.firstName}</h3>
->>>>>>> 032f914974408b1a7dd5cb7910b4e231deabc3c5
           </div>
         </div>
         <div className={classes.DetailsContainer}>
@@ -65,11 +57,7 @@ const UpcomingBirthdaySummary = (props) => {
 
 UpcomingBirthdaySummary.propTypes = {
   id: PropTypes.string,
-<<<<<<< HEAD
   name: PropTypes.string.isRequired,
-=======
-  firstName: PropTypes.string.isRequired,
->>>>>>> 032f914974408b1a7dd5cb7910b4e231deabc3c5
   img: PropTypes.string,
   birthday: PropTypes.instanceOf(Date),
   onClick: PropTypes.func.isRequired,
